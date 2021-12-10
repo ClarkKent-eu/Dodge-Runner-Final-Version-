99dfb51f0f772ea89f4ab34c8332d2e23a1b9b3c
ClarkKent-eu <74181748+ClarkKent-eu@users.noreply.github.com>
2021-12-10 09:24:19 +0000

    renaming scripts

--- Dodge Runner/Assets/EnemyMove.cs
+++ Dodge Runner/Assets/EnemyMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRight : MonoBehaviour
+public class EnemyMove : MonoBehaviour
 {
     private float speed = 10;
     // Start is called before the first frame update
